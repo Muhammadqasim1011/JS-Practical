272020e635af86cd827f857a9f37d6cad8989307
Muhammadqasim1011 <akhundqasim1011@gmail.com>
2024-07-05 10:02:21 +0500

    Fix some issue

--- JS-Practical-6/All-Asignment.js
+++ JS-Practical-6/All-Asignment.js
@@ -29,26 +29,31 @@ let joinString = array.join(' ');
 console.log(joinString);
 
 // Task- 6: Find the index of the character "o" in the string: "Hello World"
+
 let string6 = "Hello World";
 let index = string6.indexOf('o');
 console.log(index);
 
 // Tassk-7: Find the last index of the substring "World" in the string: "Hello World World"
+
 let string7 = "Hello World World";
 let lastIndex = string7.lastIndexOf('World');
 console.log(lastIndex);
 
 // Task-8: Check if the string "World" starts with "Wor"
+
 let string8 = "World";
 let check = string8.startsWith('Wor');
 console.log(check);
 
 // Task-9: Check if the string "Hello World" ends with "World"
+
 let string9 = "Hello World";
 let check1 = string9.endsWith('World');
 console.log(check1);
 
 // Task-10: Check if the string "Hello" includes the character "o"
+
 let string10 = "Hello";
 let check2 = string10.includes('o');
 console.log(check2);
